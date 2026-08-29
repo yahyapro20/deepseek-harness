b7331e561eeab4e9a5907a5a3b06f55828f9bc68
yahyapro20 <hosseiniyahya96@gmail.com>
2026-08-29 21:37:49 +0330

    Delete mobile directory

--- mobile/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-// Top-level build file. No project-wide dependencies needed for the MVP.
-plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-}
